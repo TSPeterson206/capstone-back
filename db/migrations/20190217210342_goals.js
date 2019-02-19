a5aa860f80c8433f77634a2b75d909b63c9c4799
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-02-18 20:35:12 -0800

    added enddate to seeds, migrations and MVC for goals

--- db/migrations/20190217210342_goals.js
+++ db/migrations/20190217210342_goals.js
@@ -3,14 +3,10 @@ exports.up = function(knex, Promise) {
       table.increments();
       table.integer('user_id').references('users.id').onDelete('CASCADE')
       table.text('goal').notNullable().defaultsTo('');
+      table.text('enddate').notNullable().defaultsTo('');
+
       table.timestamps(true, true);
   })
-  // .then(t => {
-  //   return knex.schema.raw(
-  //     `ALTER TABLE "goals"
-  //     ADD CONSTRAINT "goals_unique" UNIQUE(user_id)`
-  //   )
-  // })
 };
 
 exports.down = function(knex, Promise) {
