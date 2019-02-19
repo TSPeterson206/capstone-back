adbe94cf784383063c6578068b56974d6a67e061
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-02-19 10:06:09 -0800

    changed up seeds for favorites to remove dupes

--- db/seeds/04_favorites.js.js
+++ db/seeds/04_favorites.js.js
@@ -20,17 +20,17 @@ exports.seed = function(knex, Promise) {
     {id: 18, user_id:8, provider_id:18},
     {id: 19, user_id:9, provider_id:19},
     {id: 20, user_id:10, provider_id:20},
-    {id: 21, user_id:1, provider_id:1},
-    {id: 22, user_id:2, provider_id:2},
-    {id: 23, user_id:3, provider_id:3},
-    {id: 24, user_id:4, provider_id:4},
-    {id: 25, user_id:5, provider_id:5},
-    {id: 26, user_id:6, provider_id:6},
-    {id: 27, user_id:7, provider_id:7},
-    {id: 28, user_id:8, provider_id:8},
-    {id: 29, user_id:9, provider_id:9},
+    {id: 21, user_id:1, provider_id:19},
+    {id: 22, user_id:2, provider_id:18},
+    {id: 23, user_id:3, provider_id:17},
+    {id: 24, user_id:4, provider_id:16},
+    {id: 25, user_id:5, provider_id:15},
+    {id: 26, user_id:6, provider_id:14},
+    {id: 27, user_id:7, provider_id:13},
+    {id: 28, user_id:8, provider_id:12},
+    {id: 29, user_id:9, provider_id:11},
     {id: 30, user_id:10, provider_id:10},
-    {id: 31, user_id:1, provider_id:11}
+    {id: 31, user_id:1, provider_id:9}
 
     ])
   .then(() => {
