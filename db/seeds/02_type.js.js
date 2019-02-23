185e2ab860435863710191a152313ce510ed5c72
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-02-22 22:52:54 -0800

    added fitness to seeds

--- db/seeds/02_type.js.js
+++ db/seeds/02_type.js.js
@@ -4,7 +4,8 @@ exports.seed = function(knex, Promise) {
     {id: 2, description:"medical"} ,
     {id: 3, description:"legal"} ,
     {id: 4, description:"mh"} ,
-    {id: 5, description:"financial"}
+    {id: 5, description:"financial"},
+    {id:6, description:"fitness"}
   ])
   .then(() => {
     return knex.raw("SELECT setval('type_id_seq', (SELECT MAX(id) FROM type));")
